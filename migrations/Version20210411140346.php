886d57b235b118f253cc5ce79b5213adb124ef70
Branimir97 <branimirb51@gmail.com>
2021-04-13 20:10:11 +0200

    Fixed one migration for item creation

--- migrations/Version20210411140346.php
+++ migrations/Version20210411140346.php
@@ -21,13 +21,11 @@ final class Version20210411140346 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE color (id INT AUTO_INCREMENT NOT NULL, value VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE item CHANGE ref_number cipher VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE color');
-        $this->addSql('ALTER TABLE item CHANGE cipher ref_number VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
