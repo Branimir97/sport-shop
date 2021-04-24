679623e8dd380de2cd84b3c0484c26e6d5ffea08
Branimir97 <branimirb51@gmail.com>
2021-04-24 09:33:18 +0200

    Generated data for fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Color;
+use App\Entity\Size;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +13,125 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $user = new User();
+        $user->setName("Branimir");
+        $user->setSurname("Butković");
+        $user->setEmail("branimir@gmail.com");
+        $user->setRoles(["ROLE_USER", "ROLE_ADMIN"]);
+        $user->setBirthDate(new \DateTime("1997-10-27"));
+        $user->setGender("Muški");
+        $user->setPassword('$argon2id$v=19$m=65536,t=4,p=1$avj5HbJl56Te5US1YZiQAQ$T9qpVqy9QHEokQZya9zJLjpHsS0pqh8aqFRHZkKcMOI');
+
+        $categories = [
+            "Muškarci",
+            "Žene",
+            "Djeca",
+            "Sportska oprema",
+            "Ostalo",
+            "Obuća",
+            "Odjeća",
+            "Tenisice",
+            "Kopačke",
+            "Patike za trčanje",
+            "Čizme",
+            "Dodaci za obuću",
+            "Ulošci",
+            "Vezice",
+            "Impregnacijski sprejevi",
+            "Majice",
+            "Hlače",
+            "Jakne",
+            "Odjevni dodaci",
+            "Kape",
+            "Rukavice",
+            "Šalovi",
+            "Nogomet",
+            "Rukomet",
+            "Košarka",
+            "Odbojka",
+            "Stolni tenis",
+            "Steznici",
+            "Znojnici",
+            "Ruksaci",
+            "Čarape"
+        ];
+
+        $sizesClothes = [
+            "XS",
+            "S",
+            "M",
+            "L",
+            "XL",
+            "XXL",
+            "XXXL"
+        ];
+
+        $sizesFootWear = [
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+        ];
+
+        $colors = [
+            "Crna",
+            "Bijela",
+            "Crvena",
+            "Svijetlo crvena",
+            "Zelena",
+            "Svijetlo zelena",
+            "Plava",
+            "Svijetlo plava",
+            "Siva",
+            "Narančasta",
+            "Žuta",
+            "Svijetlo žuta",
+            "Ljubičasta",
+            "Tirkizna"
+        ];
+
+        $manager->persist($user);
+
+        foreach($categories as $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+        }
+
+        foreach($sizesClothes as $size) {
+            $sizeObj = new Size();
+            $sizeObj->setValue($size);
+            $sizeObj->setType("Odjeća");
+            $manager->persist($sizeObj);
+        }
+
+        foreach($sizesFootWear as $size) {
+            $sizeObj = new Size();
+            $sizeObj->setValue($size);
+            $sizeObj->setType("Obuća");
+            $manager->persist($sizeObj);
+        }
+
+        foreach($colors as $color) {
+            $colorObj = new Color();
+            $colorObj->setValue($color);
+            $manager->persist($colorObj);
+        }
 
         $manager->flush();
     }
