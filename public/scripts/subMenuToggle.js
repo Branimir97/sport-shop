97a7bee4c680a342b1455c258349dc52a3aad2f1
Branimir97 <branimirb51@gmail.com>
2021-05-18 21:40:42 +0200

    Fixed sidebar collapse

--- public/scripts/subMenuToggle.js
+++ public/scripts/subMenuToggle.js
@@ -1,5 +1,5 @@
 <!-- SubMenu Toggle Script -->
-$("#menu-toggle").click(function(e) {
+$("#menu2-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
