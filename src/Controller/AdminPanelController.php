e826041d9b75e48a4e2fbd604c5b24667e4b4588
Branimir97 <branimirb51@gmail.com>
2021-06-11 17:26:00 +0200

    Adding features in admin panel

--- src/Controller/AdminPanelController.php
+++ src/Controller/AdminPanelController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\OrderListItemRepository;
+use App\Repository\ReviewRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +14,38 @@ class AdminPanelController extends AbstractController
     /**
      * @Route("/admin/panel", name="admin_panel")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository, OrderListItemRepository $orderListItemRepository, ReviewRepository $reviewRepository): Response
     {
+        $usersNumber = 0;
+        $adminsNumber = 0;
+        $soldItems = 0;
+        $earnings = 0;
+        $users = $userRepository->findAll();
+        foreach($users as $user) {
+            if(in_array("ROLE_ADMIN", $user->getRoles())){
+                $adminsNumber++;
+            } else {
+                $usersNumber++;
+            }
+        }
+
+        $orderListItems = $orderListItemRepository->findAll();
+        $orders = count($orderListItems);
+        foreach($orderListItems as $orderListItem) {
+            foreach($orderListItem->getOrderItem() as $orderItem) {
+                $soldItems+=$orderItem->getQuantity();
+            }
+            $earnings+=$orderListItem->getTotalPrice();
+        }
         return $this->render('admin_panel/index.html.twig', [
-            'controller_name' => 'AdminPanelController',
+            'users'=>$usersNumber,
+            'admins'=>$adminsNumber,
+            'soldItems'=>$soldItems,
+            'orders'=>$orders,
+            'earnings'=>$earnings,
+            'last10orderListItems'=>$orderListItemRepository->findBy([], ['id'=>'DESC'], 10),
+            'last5registeredUsers'=>$userRepository->findBy([], ['id'=>'DESC'], 5),
+            'last5reviews'=>$reviewRepository->findBy([], ['id'=>'DESC'], 5)
         ]);
     }
 }
