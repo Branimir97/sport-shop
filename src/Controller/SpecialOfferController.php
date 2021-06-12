8ac7d1e7b8a0e8c6406a130b0f0d5a07d7329980
Branimir97 <branimirb51@gmail.com>
2021-06-12 21:29:24 +0200

    Done special offers

--- src/Controller/SpecialOfferController.php
+++ src/Controller/SpecialOfferController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ActionCategoryRepository;
+use App\Repository\ActionItemRepository;
+use App\Repository\ItemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +14,32 @@ class SpecialOfferController extends AbstractController
     /**
      * @Route("/akcijska/ponuda", name="special_offer")
      */
-    public function index(): Response
+    public function index(ActionItemRepository $actionItemRepository,
+                          ItemRepository $itemRepository): Response
     {
-        return $this->render('special_offer/index.html.twig');
+
+        $items = [];
+        $discounts = [];
+        $actionItems = $actionItemRepository->findAll();
+        $itemsDb = $itemRepository->findAll();
+        foreach($actionItems as $actionItem) {
+            array_push($items, $actionItem->getItem());
+            array_push($discounts, $actionItem->getDiscountPercentage());
+        }
+        foreach ($itemsDb as $item) {
+            $itemCategories = $item->getItemCategories();
+            foreach($itemCategories as $itemCategory) {
+                if(!is_null($itemCategory->getCategory()->getActionCategory())) {
+                    array_push($items, $item);
+                    array_push($discounts, $itemCategory->getCategory()->getActionCategory()
+                        ->getDiscountPercentage());
+                }
+            }
+        }
+
+        return $this->render('special_offer/index.html.twig', [
+            'items'=>$items,
+            'discounts'=>$discounts
+        ]);
     }
 }
