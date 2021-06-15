8d93d5b4aa03d37d801e9a4a242d9876d19aa81a
Branimir97 <branimirb51@gmail.com>
2021-06-15 19:20:27 +0200

    Added language icon + dropdown menu

--- src/Controller/ItemListController.php
+++ src/Controller/ItemListController.php
@@ -3,16 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ItemListController extends AbstractController
 {
     /**
-     * @Route("/lista/artikala", name="item_list")
+     * @Route("/itemi/{categories}", name="item_list")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $categories = $request->get('categories');
+        dd($categories);
         return $this->render('item_list/index.html.twig');
     }
 }
