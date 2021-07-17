4c299fb5f3bdfd91554e8d1706947f6a8e611542
Branimir97 <branimirb51@gmail.com>
2021-07-17 18:02:03 +0200

    Changed timezone | upgraded recommendation system

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\ItemRepository;
 use App\Repository\UserSearchRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,14 +17,31 @@ class HomeController extends AbstractController
      *     "hr": "/"
      * }, name="homepage")
      */
-    public function index(ItemRepository $itemRepository, UserSearchRepository $userSearchRepository): Response
+    public function index(Request $request,
+                          ItemRepository $itemRepository,
+                          UserSearchRepository $userSearchRepository): Response
     {
         $suggestedItems = [];
         $suggestedItemIds = [];
         $randomSuggestedItems = [];
+        $categories = [];
         if($this->getUser()) {
-            $userSearch = $userSearchRepository->findOneBy(['user' => $this->getUser()], ['id' => 'DESC']);
-            $suggestedItems = $itemRepository->findSuggestedItems($this->getUser()->getGender(), $userSearch);
+            $userSearch = $userSearchRepository->findOneBy(
+                ['user' => $this->getUser()], ['id' => 'DESC']);
+            if(!is_null($userSearch)) {
+                $searchResults = $itemRepository
+                    ->searchByCipherAndName($userSearch->getKeyword(), $request->getLocale());
+
+                foreach($searchResults->getQuery()->getResult() as $item) {
+                    $itemCategories = $item->getItemCategories();
+                    foreach($itemCategories as $itemCategory) {
+                        array_push($categories, $itemCategory->getCategory()->getName());
+                    }
+                }
+               $categories = array_values(array_unique($categories));
+            }
+            $suggestedItems = $itemRepository
+                ->findSuggestedItems($this->getUser()->getGender(), $categories);
         }
 
         if(!is_null($suggestedItems)) {
@@ -34,9 +52,11 @@ class HomeController extends AbstractController
             }
             if(count($suggestedItemIds) > 0) {
                 if(count($suggestedItemIds) >= 4) {
-                    $randomSuggestedItemIds = array_rand($suggestedItemIds, 4);
+                    $randomSuggestedItemIds =
+                        array_rand($suggestedItemIds, 4);
                 } else {
-                    $randomSuggestedItemIds = array_rand($suggestedItemIds, count($suggestedItemIds));
+                    $randomSuggestedItemIds =
+                        array_rand($suggestedItemIds, count($suggestedItemIds));
                 }
                 if((count($suggestedItemIds)) == 1) {
                     array_push($randomSuggestedItems,
